00a58ba58a599ee40b36b9d7bb806d2ba2abcb20
Mark McCarthy <fiahdrgn473@gmail.com>
2024-10-08 19:42:32 -0400

    save

--- blog-service.js
+++ blog-service.js
@@ -1,6 +1,18 @@
 const fs = require("fs");
+const path = require('path');
+const categories = JSON.parse(path.join(__dirname, '/data/categories.json'));
+const posts = JSON.parse(path.join(__dirname, '/data/categories.json'));
 
-fs.readFile('somefile.json', 'utf8', (err, data) => {
+fs.readFile('categories.json', 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
     });
+
+function outputA() {
+    return new Promise((resolve, reject) => {
+        // place our code inside a "Promise" function
+        console.log('A');
+        resolve(); // call "resolve" because we have completed the function successfully
+        reject();
+    });
+}
