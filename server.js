06df223af91dc6e2e7193a21e2b7e5be6438ede2
Mark McCarthy <fiahdrgn473@gmail.com>
2024-10-08 22:47:13 -0400

    routes do stuff now

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const { initialize } = require('./blog-service');
+const blogservice = require('/blog-service');
 //const blog = require('blog-service.js')
 
 const HTTP_PORT = process.env.PORT || 8080;
@@ -10,17 +10,23 @@ app.get('/', (req, res) => {
   res.redirect('/about');
 });
 
-app.get('/blog', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/about'));
+app.get('/posts', (req, res) => {
+  blogservice.getAllPosts()
+  .then(data=> res.json(data))
+  .catch(err => res.status(404).json({ message: err }));
 });
 
-app.get('/posts', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/about.html'));
-  });
-  
+app.get('/blog', (req, res) => {
+  blogservice.getPublishedPosts()
+  .then(data=> res.json(data))
+  .catch(err => res.status(404).json({ message: err }));
+});
+
 app.get('/categories', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/about.html'));
-  });
+  blogservice.getPublishedPosts()
+  .then(data=> res.json(data))
+  .catch(err => res.status(404).json({ message: err }));
+});
   
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/about.html'));
@@ -34,10 +40,12 @@ app.use((req, res, next)  => {
     res.redirect('/Error404');
 });
 
-initialize().then(() => {
+blogservice.initialize().then(() => {
 app.listen(HTTP_PORT, () => {
   console.log(`server listening on: ${HTTP_PORT}`);
 });
-}).catch(() => {
-  console.log({message: err})
+})
+.catch(err => {
+  console.log({message: err});
+  process.exit(1);
 });
