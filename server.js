dac2006f9923a634f962e048bb617aafcade1855
Mark McCarthy <fiahdrgn473@gmail.com>
2024-10-08 22:48:26 -0400

    .js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const blogservice = require('/blog-service');
+const blogservice = require('/blog-service.js');
 //const blog = require('blog-service.js')
 
 const HTTP_PORT = process.env.PORT || 8080;
