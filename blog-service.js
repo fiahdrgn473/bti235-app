d407f2affd4f99682ef6fdcf0f2a42c4c4e0bec7
Mark McCarthy <fiahdrgn473@gmail.com>
2024-10-09 15:47:17 -0400

    teacher ver

--- blog-service.js
+++ blog-service.js
@@ -4,8 +4,8 @@ let categories = [];
 let posts = [];
 
 function initialize() {
-    return new Promise((resolve, reject) => {
 
+    const promise1 = new Promise((resolve, reject) => {
         fs.readFile(path.join(__dirname, './data/posts.json'), 'utf8', (err, data) => {
             if (err) {
                 return reject('unable to read file posts')
@@ -15,19 +15,32 @@ function initialize() {
             } catch (parseErr) {
                 return reject('unable to parse posts');
             }
-            fs.readFile(path.join(__dirname, './data/categories.json'), 'utf8', (err, data) => {
-                if (err) {
-                    return reject('unable to read file categories')
-                }
-                try {
-                    categories = JSON.parse(data);
-                } catch (parseErr) {
-                    return reject('unable to parse categories');
-                }
-                resolve();
-            });
-        });
-    });
+            resolve()
+        })
+    })
+
+    const promise2 = new Promise((resolve, reject) => {
+        fs.readFile(path.join(__dirname, './data/categories.json'), 'utf8', (err, data) => {
+            if (err) {
+                return reject('unable to read file categories')
+            }
+            try {
+                categories = JSON.parse(data);
+            } catch (parseErr) {    
+                return reject('unable to parse categories');
+            }
+            resolve()
+        })
+    })
+
+    return new Promise((resolve, reject) => {
+        Promise.all([promise1, promise2]).then(() => {
+            resolve()
+        }).catch(err => {
+            reject(err)
+        })
+    })
+
 }
 function getAllPosts(){
     return new Promise((resolve, reject) => {
